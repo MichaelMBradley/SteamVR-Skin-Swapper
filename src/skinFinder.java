8067b71dc455aa905900955faa2ee47466d7fe78
Michael Bradley <michaelmbrad@gmail.com>
2020-07-26 13:33:36 -0400

    Add first time setup

--- src/skinFinder.java
+++ src/skinFinder.java
@@ -1,5 +1,9 @@
+import java.io.*;
+import java.util.*;
+
 public class skinFinder {
 	public static void main(String[] args) {
-		
+		File SteamVR = new File("C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\250820");
+		ArrayList<String[]> skins = new ArrayList<String[]>();
 	}
 }
