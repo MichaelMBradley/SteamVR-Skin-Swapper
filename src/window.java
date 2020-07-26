fecf3e6d50456c3ecd20f5c360828270e3b197dc
Michael Bradley <michaelmbrad@gmail.com>
2020-07-26 19:49:39 -0400

    Add improvements to preview

--- src/window.java
+++ src/window.java
@@ -1,3 +1,4 @@
+import java.awt.Desktop;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
@@ -20,6 +21,7 @@ public class window implements ActionListener {
 	private String[][] skinfo;
 	private String message;
 	private String steam;
+	private String currentImg;
 	
 	public void info(String SteamVR, String[][] skin, String msg) {
 		dir = SteamVR + "\\steamapps\\workshop\\content\\250820";
@@ -36,7 +38,7 @@ public class window implements ActionListener {
 		l = new JButton("<");
 		l.setBounds(0, 0, 50, 300);
 		l.addActionListener(this);
-		e = new JButton("Exit");
+		e = new JButton("Explorer");
 		e.setBounds(0, 300, 100, 50);
 		e.addActionListener(this);
 		c = new JButton("Choose");
@@ -90,7 +92,11 @@ public class window implements ActionListener {
 			}
 			reloadImage();
 		} else if(a.getSource() == e) {
-			System.exit(0);
+			try {
+				Desktop.getDesktop().open(new File(currentImg).getParentFile());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		} else if(a.getSource() == c) {
 			if(type == 0) {
 				skinChanger.swapSkins(type, indb, skinfo, steam);
@@ -114,7 +120,8 @@ public class window implements ActionListener {
 			if(type == 0) {
 				if(Integer.valueOf(skinfo[0][0])!=0) {
 					File[] sub = new File(dir + "\\" + skinfo[0][indb]).listFiles();
-					BufferedImage img = ImageIO.read(new File(sub[0] + "\\vive_base_thumbnail.png"));
+					currentImg = sub[0] + "\\vive_base_thumbnail.png";
+					BufferedImage img = ImageIO.read(new File(currentImg));
 					ImageIcon icon = new ImageIcon(img);
 					j.setIcon(icon);
 				} else {
@@ -123,7 +130,8 @@ public class window implements ActionListener {
 			} else {
 				if(Integer.valueOf(skinfo[1][0])!=0) {
 					File[] sub = new File(dir + "\\" + skinfo[1][indc]).listFiles();
-					BufferedImage img = ImageIO.read(new File(sub[0] + "\\vive_controller_thumbnail.png"));
+					currentImg = sub[0] + "\\vive_controller_thumbnail.png";
+					BufferedImage img = ImageIO.read(new File(currentImg));
 					ImageIcon icon = new ImageIcon(img);
 					j.setIcon(icon);
 				} else {
